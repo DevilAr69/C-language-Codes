4f6435f8f2f95133f7af835bdf97709da1ef51b4
DevilAr69 <131570647+DevilAr69@users.noreply.github.com>
2023-09-12 10:31:22 +0530

    Add files via upload

---
+++ ascii value.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+    for ( i = 0; i<=127; i++)
+    {
+        printf("\n %d =%c",i,i);
+
+    }
+    
+    
+}
---
+++ fac.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    int f=1;
+    printf("enter any number :");
+    scanf("%d",&n);
+    for ( i = 1; i <=n; i++)
+    {
+        f=f*i;
+        
+
+        /* code */
+    }
+    printf("\nfac = %d",f);
+}
